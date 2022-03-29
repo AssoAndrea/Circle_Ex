b70772908f01b42016f3970dc1d17dffe69c3de4
AssoAndrea <andrea1944@live.it>
2022-03-29 20:10:13 +0200

    fix

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import graph
 
 if __name__ == '__main__':
     c1 = graph.MyCircle(1, 1, 0.2,color="blue")
-    c2 = graph.MyCircle(0.1, 2, 0.4)
+    c2 = graph.MyCircle(0.1, 2, 0.4,color="violet")
     c3 = c1 + c2
     print(c3>c1)
     print(c3==c1)
